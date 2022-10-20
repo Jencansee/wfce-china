658486ea801cb4f952881f45fb92f790db925287
Schneider <jncxnsee@tuta.io>
2022-10-21 04:16:16 +0500

    __app.tsx update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,48 @@
+import AllowCookies from '@components/Cookies/Cookies';
+import Feedback from '@components/Feedback/Feedback';
+import Footer from '@components/Footer/Footer';
+import Header from '@components/Header/Header';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode, useEffect, useState } from 'react';
 import GlobalStyles from '../styles/GlobalStyles'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type ComponentWithPageLayout = AppProps & {
+	Component: NextPageWithLayout
+};
+
+
+function MyApp({ Component, pageProps }: ComponentWithPageLayout) {
+	const [allowCookies, setAllowCookies] = useState(false);
+
+	const getLayout = Component.getLayout ?? ((page) => page)
+
+	const handleAllowCookies = () => {
+		localStorage.setItem('allowCookies', 'true');
+		setAllowCookies(true);
+	};
+
+	useEffect(() => {
+		let cookiesPermission = localStorage.getItem('allowCookies');
+		
+		if (cookiesPermission === 'true') setAllowCookies(true);
+	}, [allowCookies]);
+
+
+	return (
 		<>
-			<Component {...pageProps} />
 			<GlobalStyles />
+			<AllowCookies allowCookies={allowCookies} handleAllowCookies={handleAllowCookies} />
+			<Header />
+
+			{ getLayout(<Component {...pageProps} />) }
+
+			<Feedback toggleModal={() => console.log('weqwe')} />
+			<Footer />
 		</>
 	)
 };
